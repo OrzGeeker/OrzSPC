34106b0cd7240f3fca099d80b41207c02f0367d1
wangzhizhou <wangzhizhou@bytedance.com>
2023-12-05 02:27:10 +0800

    generate collection successfully

--- generate.sh
+++ generate.sh
@@ -2,7 +2,15 @@
 
 git pull --recurse-submodules
 
+input_path=collection_input_file.json
+
+output_path=OrzGeeker.json
+
 swift run --package-path swift-package-collection-generator \
-    package-collection-generate \
-    collection_input_file.json \
-    OrzGeeker.json
+    package-collection-generate --pretty-printed \
+    ${input_path} \
+    ${output_path}
+
+swift run --package-path swift-package-collection-generator \
+    package-collection-validate \
+    ${output_path}
